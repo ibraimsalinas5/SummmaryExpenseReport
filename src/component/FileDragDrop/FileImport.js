cb2c8a138a6d2102e95eb192be7347dfbc45803e
Ibraim Salinas <ibraimsalinas1@gmail.com>
2023-05-23 20:22:07 -0500

    Added functionality to do calculations for multiple files

--- src/component/FileDragDrop/FileImport.js
+++ src/component/FileDragDrop/FileImport.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useState } from "react";
+import FileImport from "./FileDragDrop/FileImport";
+import CalculateExpenses from "../../calculationFunctions/CalculateExpenses";
+
 export default function FileImport() {
-  // drag state
   const [dragActive, setDragActive] = useState(false);
   const [files, addFiles] = useState();
 
-  // handle drag events
-  const handleDrag = function(e){
+  let handleDrag = (e) =>{
     e.preventDefault();
     e.stopPropagation();
     if (e.type === "dragenter" || e.type === "dragover") {
@@ -16,7 +17,7 @@ export default function FileImport() {
     }
   };
 
-  const handleDrop = function(e) {
+  let handleDrop = (e) => {
     e.preventDefault();
     e.stopPropagation();
     setDragActive(false);
@@ -28,13 +29,36 @@ export default function FileImport() {
     }
   };
 
+  let handleSubmit = (files) => {
+    let filesData = [];
+    Promise.all([...files].map((file) =>
+    new Promise((resolve, reject) =>
+        parse(file, {
+        header: true,
+        complete: resolve,  // Resolve each promise
+        error: reject,
+      }),
+    )),
+  ).then((results) => {
+    results.forEach((result, index) => {
+      filesData.push(result)
+    })
+        doSomething(filesData)
+  }).catch((err) => console.log('Something went wrong:', err))
+    let data = CalculateExpenses(filesData.data);   
+     setTotalExpenses(data.totalExpenses);
+     setCategories(data.categories);
+     setCategoryExpenses(data.categoryExpenses);
+     setRender(true);
+  };
+
     return (
     <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
         <input type="file" id="input-file-upload" multiple={true} />
         <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
           <div>
-            <p>Drag and drop your file here or</p>
-            <button className="upload-button">Upload a file</button>
+            <p>Drag and drop your files here</p>
+            <button className="upload-button" onSubmit={handleSubmit}>Click To Calculate expense</button>
           </div> 
         </label>
         { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag}  onDrop={handleDrop}></div> }
